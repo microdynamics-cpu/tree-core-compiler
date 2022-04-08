1dd8ac9a65eb3923a13dc8d4be6c8b5a55b62f5d
maksyuki <maksyuki@126.com>
2022-04-08 10:48:31 +0800

    chore: add needed perm statement

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-apt-get update
-apt-get install qemu-user
+sudo apt-get update
+sudo apt-get install qemu-user
 qemu-riscv64 --version
 
-wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-export PATH=$PATH:$pwd/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
-source ~/.bashrc
+# wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+# tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+# export PATH=$PATH:$pwd/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
+# source ~/.bashrc
 
-riscv64-unknown-elf-gcc --version
+# riscv64-unknown-elf-gcc --version
