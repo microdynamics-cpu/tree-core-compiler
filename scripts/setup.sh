9f8740e13c6380e43713fcba4cbcf38f66d748d9
maksyuki <maksyuki@126.com>
2022-04-08 10:51:29 +0800

    chore: add riscv toolchain statement to bash env config

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,8 @@ qemu-riscv64 --version
 
 # wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 # tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-# export PATH=$PATH:$pwd/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
+export PATH=$PATH:$(pwd)/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
+cat ~/.bashrc
 # source ~/.bashrc
 
 # riscv64-unknown-elf-gcc --version
