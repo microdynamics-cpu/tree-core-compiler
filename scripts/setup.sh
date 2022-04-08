5a53ed743fb43506c8b542961c692dbd2e9f87ab
maksyuki <maksyuki@126.com>
2022-04-08 10:54:47 +0800

    chore: add riscv toolchain verify step

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,10 +4,9 @@ sudo apt-get update
 sudo apt-get install qemu-user
 qemu-riscv64 --version
 
-# wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-# tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-export PATH=$PATH:$(pwd)/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
-cat ~/.bashrc
-# source ~/.bashrc
+wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+echo export PATH=$PATH:$(pwd)/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
+source ~/.bashrc
 
-# riscv64-unknown-elf-gcc --version
+riscv64-unknown-elf-gcc --version
