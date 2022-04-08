d4d5363bd693c8ab1c4572d3c2693f0f7b517749
maksyuki <maksyuki@126.com>
2022-04-08 12:55:40 +0800

    chore: remove riscv isa test action

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,7 +5,7 @@ sudo apt-get install qemu-user
 qemu-riscv64 --version
 
 wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
+tar -xf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 echo export PATH=$PATH:$(pwd)/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
 source ~/.bashrc
 
