71f7876d71d57f96fd5e64ab0d55bacdd47481fc
maksyuki <maksyuki@126.com>
2022-04-08 10:42:25 +0800

    fix: fix a perm oper bug

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,5 @@ apt-get install qemu-user
 
 wget https://static.dev.sifive.com/dev-tools/freedom-tools/v2020.08/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
 tar -xvf riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14.tar.gz
-cp riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/* /usr/ -r
+export PATH=$PATH:$pwd/riscv64-unknown-elf-gcc-10.1.0-2020.08.2-x86_64-linux-ubuntu14/bin > ~/.bashrc
+source ~/.bashrc
